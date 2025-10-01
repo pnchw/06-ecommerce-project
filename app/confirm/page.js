97a265ea6768d4455f6a09535bd0afc5ea56ccef
pnchw <pancheewa.thpm@hotmail.com>
2025-10-01 11:27:39 +0700

    forth commit - edit login-confirm-success

--- app/confirm/page.js
+++ app/confirm/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+export const dynamic = "force-dynamic";
 import { useEffect, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import { CheckCircle } from "lucide-react";
