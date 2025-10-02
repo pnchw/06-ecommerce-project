daacf642515f017e37e959d5a2a0d503e0674941
pnchw <pancheewa.thpm@hotmail.com>
2025-10-02 11:53:50 +0700

    tenth commit - fix tailwind error

--- components/Loading.js
+++ components/Loading.js
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function LoadingPage({ page }) {
 	return (
-		<div className="flex justify-center items-center h-100vh">
-			<p className="text-xl text-center mb-2">{page}</p>{" "}
+		<div className="text-center mt-20 text-xl">
+			{page}{" "}
 			<Link
 				href="/"
 				className="text-blue-600 hover:underline font-semibold"
