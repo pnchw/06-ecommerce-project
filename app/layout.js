3ef42dbc74f157e890932e9f429c190f977c9833
pnchw <pancheewa.thpm@hotmail.com>
2025-10-02 11:24:45 +0700

    ninth commit - metadata icon

--- app/layout.js
+++ app/layout.js
@@ -7,35 +7,38 @@ import Footer from "@/components/footer/Footer";
 import { Toaster } from "@/components/ui/sonner";
 
 const roboto = Roboto({
-  weight: ["300", "700"],
-  subsets: ["latin"],
+	weight: ["300", "700"],
+	subsets: ["latin"],
 });
 
 export const metadata = {
-  title: "E-Anime shop",
-  description: "Anime character e-commerce shop",
+	title: "E-Anime shop",
+	description: "Anime character e-commerce shop",
+	icons: {
+		icon: "/assets/images/logo.png",
+	},
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        <link rel="icon" type="image/png" href="/assets/images/logo.png" />
-      </head>
-      <body
-        className={`${roboto.className} antialiased bg-[rgb(249, 243, 239)]`}
-      >
-        <SessionProvider>
-          <CartProvider>
-            <div className="flex flex-col min-h-screen">
-              <Navbar />
-              <main className="flex-grow">{children}</main>
-              <Footer />
-            </div>
-            <Toaster />
-          </CartProvider>
-        </SessionProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<link rel="icon" type="image/png" href="/assets/images/logo.png" />
+			</head>
+			<body
+				className={`${roboto.className} antialiased bg-[rgb(249, 243, 239)]`}
+			>
+				<SessionProvider>
+					<CartProvider>
+						<div className="flex flex-col min-h-screen">
+							<Navbar />
+							<main className="flex-grow">{children}</main>
+							<Footer />
+						</div>
+						<Toaster />
+					</CartProvider>
+				</SessionProvider>
+			</body>
+		</html>
+	);
 }
